6fe88a9db023ac73dfc4e5566e23e3058d9af80e
Egor Vafin <post1978@gmail.com>
2024-04-15 20:47:16 +0300

    added producer

--- src/main/java/com/learn/libraryeventsproducer/controller/LibraryEventsController.java
+++ src/main/java/com/learn/libraryeventsproducer/controller/LibraryEventsController.java
@@ -1,6 +1,8 @@
 package com.learn.libraryeventsproducer.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learn.libraryeventsproducer.domain.LibraryEvent;
+import com.learn.libraryeventsproducer.producer.LibraryEventsProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LibraryEventsController {
 
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
+
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
             @RequestBody LibraryEvent libraryEvent
-    )
-    {
+    ) throws JsonProcessingException {
         log.info("libraryEvent : {} ", libraryEvent);
+
+        libraryEventsProducer.sendLibraryEvent(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
